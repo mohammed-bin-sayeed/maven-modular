19616b93e56a5b41bffb03902b7c71b4d62e6207
mohammed bin Sayeed (FINDIA.DEL) <mohammed.B@fsoft.com.vn>
2022-06-30 16:46:59 +0530

    my modified commit

---
+++ module3/src/main/java/module3/Main12.java
@@ -0,0 +1,20 @@
+package module3;
+
+import io.jitpack.Module1Util;
+import io.jitpack.Module2Util;
+
+public class Main12 {
+        public static void main(String[] args) {
+        	
+        	Module1Util m1u = new Module1Util();
+        	String result = m1u.testmethod();
+        	System.out.println(result);
+        	
+        	Module2Util m2u = new Module2Util();
+        	String result1 = m2u.testMethod();
+        	System.out.println(result1);
+        
+        
+        	
+		}
+}
